e3d0b4590bf22ffaef6b168bb51b4e901437fb47
ArielPrez <arielpgmm@gmail.com>
2018-06-30 12:33:50 -0400

    express-cinema - done

--- lab-express-cinema/starter_code/lab-express-cinema/bin/seeds.js
+++ lab-express-cinema/starter_code/lab-express-cinema/bin/seeds.js
@@ -68,7 +68,7 @@ const moviesData = [ /*<--- [moviesData] es la variable que pasas en el primer p
     stars: ["Jennifer Lawrence", "Joel Edgerton", "Matthias Schoenaerts"],
     image: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTA3MDkxOTc4NDdeQTJeQWpwZ15BbWU4MDAxNzgyNTQz._V1_UX182_CR0,0,182,268_AL_.jpg",
     description: "Ballerina Dominika Egorova is recruited to 'Sparrow School,' a Russian intelligence service where she is forced to use her body as a weapon. Her first mission, targeting a C.I.A. agent, threatens to unravel the security of both nations.",
-    howtimes: ["13:00", "15:30", "18:00", "20:10", "22:40"]
+    showtimes: ["13:00", "15:30", "18:00", "20:10", "22:40"]
   },
 ];
 
